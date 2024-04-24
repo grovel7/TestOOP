3200ff15482e7ede6555093a8d4d837c31b08be9
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2024-04-24 20:12:42 +1000

    incomplete test

---
+++ Test/Test/Batch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Test
+{
+	public class Batch
+	{
+		public Batch()
+		{
+		}
+	}
+}
+
---
+++ Test/Test/Transaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Test
+{
+	public class Transaction
+	{
+		public Transaction()
+		{
+		}
+	}
+}
+
